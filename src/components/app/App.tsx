0fecd413575226cba40fe1cfa05436a5ba241091
guiopen <guiopen.dev@gmail.com>
2025-07-16 23:22:44 -0300

    Add Prettier config and reformat codebase

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,13 @@
 import "@shared/colors.css"
-import styles from "./App.module.css";
-import MarkdownEditor from "./markdown-editor/MarkdownEditor";
+import styles from "./App.module.css"
+import MarkdownEditor from "./markdown-editor/MarkdownEditor"
 
 const App = () => {
-  return (
-    <div class={styles.page}>
-      <MarkdownEditor />
-    </div>
-  );
-};
+	return (
+		<div class={styles.page}>
+			<MarkdownEditor />
+		</div>
+	)
+}
 
-export default App;
+export default App
