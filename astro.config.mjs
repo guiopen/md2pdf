8666746a6526747bd73184f2c3fe12e514ff2de5
guiopen <guiopen.dev@gmail.com>
2025-07-21 02:30:11 -0300

    add minification and compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
 
+import playformCompress from '@playform/compress';
+
+import compressor from 'astro-compressor';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs(), playformCompress(), compressor()]
 });
