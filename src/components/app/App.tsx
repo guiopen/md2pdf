d3a09d9623372cf1828f181f970c9af845c37c4f
guiopen <guiopen.dev@gmail.com>
2025-07-22 02:21:53 -0300

    Simplify code structure by removing the App.jsx and using the index.astro directly

--- src/components/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import "@shared/colors.css"
-import "@shared/global.css"
-import MarkdownEditor from "./markdown-editor/MarkdownEditor"
-
-const App = () => {
-	return <MarkdownEditor />
-}
-
-export default App
