9996438620aad99ce55d35e27a4252ae9ee2ad55
guiopen <guiopen.dev@gmail.com>
2025-08-03 02:49:46 -0300

    Rewrite in pure astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 
-import solidJs from "@astrojs/solid-js"
-
 import playformCompress from "@playform/compress"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solidJs(), playformCompress()],
+	integrations: [playformCompress()],
 })
