e317cf1f65cb63fa13bd74cbf2f1eb48b40b382b
guiopen <guiopen.dev@gmail.com>
2025-08-03 17:03:08 -0300

    Revert "integrate i18n support for English and Portuguese"
    This reverts commit 97f5b3543affc9b9153b7b230b9761d7dd507719.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,4 @@ import playformCompress from "@playform/compress"
 // https://astro.build/config
 export default defineConfig({
 	integrations: [playformCompress()],
-	i18n: {
-		locales: ["en", "pt-br"],
-		defaultLocale: "en",
-	}
 })
